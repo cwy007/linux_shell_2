d0517d973ff7dafa2dda296d11e7eda6f0f1c7b6
chenweiyan <chanweiyan007@gmail.com>
2019-08-23 09:58:35 +0800

    printf.sh

---
+++ printf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash #文件名: printf.sh
+printf  "%-5s %-10s %-4s\n" No Name  Mark
+printf  "%-5s %-10s %-4.2f\n" 1 Sarath 80.3456
+printf  "%-5s %-10s %-4.2f\n" 2 James 90.9989
+printf  "%-5s %-10s %-4.2f\n" 3 Jeff 7.564
